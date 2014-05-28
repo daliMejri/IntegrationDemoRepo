0ce30b8f6f246b849e6bd322396c5fc5fc60746b
dali <dali@mouhamed.esprit.tn>
2014-05-28 15:21:06 +0200

    aaaaa
    Signed-off-by: dali <dali@mouhamed.esprit.tn>

--- integration-demo/src/main/java/edu/esprit/integration/demo/managed/bean/CalculatorManagedBean.java
+++ integration-demo/src/main/java/edu/esprit/integration/demo/managed/bean/CalculatorManagedBean.java
@@ -1,5 +1,36 @@
 package edu.esprit.integration.demo.managed.bean;
 
+import javax.annotation.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.naming.Name;
+
+
+import edu.esprit.integration.demo.services.*;
+@ManagedBean( value = "CalculatorBean")
+@SessionScoped
 public class CalculatorManagedBean {
+	@javax.ejb.EJB
+	private CalculatorService CalculatorService;
+
+	public int clickoperation(int val1, String operation,  int val2) {
+		if (operation == "+")
+		{
+		return CalculatorService.Add(val1, val2);
+		}
+		if (operation == "-")
+		{
+		return CalculatorService.Soustrct(val1, val2);
+		}
+		if (operation == "/")
+		{
+		return CalculatorService.div(val1, val2);
+		}
+		if (operation == "*")
+		{
+		return CalculatorService.multiplie(val1, val2);
+		}
+		else
+		return 0 ;
+	}
 
 }
