1220fa280cca26ca4d7bf5a3dd75dbeb805c76dc
dali <dali@mouhamed.esprit.tn>
2014-05-28 14:42:53 +0200

    ss
    Signed-off-by: dali <dali@mouhamed.esprit.tn>

--- integration-demo/src/main/java/edu/esprit/integration/demo/services/CalculatorService.java
+++ integration-demo/src/main/java/edu/esprit/integration/demo/services/CalculatorService.java
@@ -1,25 +1,27 @@
 package edu.esprit.integration.demo.services;
 
+
+
 public class CalculatorService {
 	
 	public int Add (int n, int m) {
 		
-			return n + m;
+			return n+m;
 
 	}
 	public int Soustrct (int n, int m) {
 		
-		return n - m;
+		return n-m;
 
 }
 public int div (int n, int m) {
 		
-		return n / m;
+		return n/m;
 
 }
 public int multiplie (int n, int m) {
 	
-	return n * m;
+	return n*m;
 
 }
 
